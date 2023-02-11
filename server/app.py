277966ebd356088657b5d8c81dfc8cf690412b3e
electromop <user4567859@gmail.com>
2023-02-11 18:51:05 +0300

    delete previous fersion fo condition dict

--- server/app.py
+++ server/app.py
@@ -1,6 +1,21 @@
 from flask import Flask, jsonify, render_template
 
-conditions = {'curtains': False}
+kitchenroom = {
+    'lights':False, 
+    'curtains':False, 
+    'temp':None
+    }
+
+bedroom = {
+    'lights':False,
+    'curtains':False,
+    'temp':None
+    }
+livingroom = {
+    'lights':False, 
+    'curtains':False, 
+    'temp':None
+    }
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
@@ -8,9 +23,47 @@ app = Flask(__name__, template_folder='templates', static_folder='static')
 def index():
     return render_template('index.html')
 
-@app.route('/api/curtains_stat', methods=['GET'])
+#########################KITCHENROOM###########################
+@app.route('/api/kitchenroom/lights', methods=['GET'])
+def lights():
+    return jsonify({'lights':kitchenroom['lights']})
+
+@app.route('/api/kitchenroom/curtains', methods=['GET'])
 def curtains():
-    return jsonify({'curtains':conditions['curtains']})
+    return jsonify({'curtains':kitchenroom['curtains']})
+
+@app.route('api/bedroom/temp', methods=['GET'])
+def temp():
+    return jsonify({'temp':kitchenroom['temp']})
+
+#########################BEDROOM###########################
+@app.route('/api/bedroom/lights', methods=['GET'])
+def lights():
+    return jsonify({'lights':bedroom['lights']})
+
+@app.route('/api/bedroom/curtains', methods=['GET'])
+def curtains():
+    return jsonify({'curtains':bedroom['curtains']})
+
+@app.route('/api/bedroom/temp', methods=['GET'])
+def temp():
+    return jsonify({'temp':bedroom['temp']})
+
+
+#########################LIVINGROOM###########################
+@app.route('/api/livingroom/lights', methods=['GET'])
+def lights():
+    return jsonify({'lights':livingroom['lights']})
+
+@app.route('/api/livingroom/curtains', methods=['GET'])
+def curtains():
+    return jsonify({'curtains':livingroom['curtains']})
+
+@app.route('/api/livingroom/temp', methods=['GET'])
+def temp():
+    return jsonify({'temp':bedroom['temp']})
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port= 8090)
