0963b077924505998998220f274348849ca9d167
electromop <user4567859@gmail.com>
2023-02-11 23:03:45 +0300

    added python vars into index.html

--- server/app.py
+++ server/app.py
@@ -3,67 +3,59 @@ from flask import Flask, jsonify, render_template
 kitchenroom = {
     'lights':False, 
     'curtains':False, 
-    'temp':None
+    'temp':1
     }
 
 bedroom = {
     'lights':False,
     'curtains':False,
-    'temp':None
+    'temp':0
     }
 livingroom = {
     'lights':False, 
     'curtains':False, 
-    'temp':None
+    'temp': 0
     }
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
+def bool_to_str(condition):
+    if condition == False:
+        return 'disabled'
+    else:
+        return 'enabled'
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template(
+        'index.html',
+        livingroom_curt_var=bool_to_str(livingroom['curtains']),
+        livingroom_lights_var=bool_to_str(livingroom['lights']),
+        livingroom_temp_var=livingroom['temp'],
 
-#########################KITCHENROOM###########################
-@app.route('/api/kitchenroom/lights', methods=['GET'])
-def lights():
-    return jsonify({'lights':kitchenroom['lights']})
+        bedroom_curt_var=bool_to_str(bedroom['curtains']),
+        bedroom_lights_var=bool_to_str(bedroom['lights']),
+        bedroom_temp_var=['temp'],
 
-@app.route('/api/kitchenroom/curtains', methods=['GET'])
-def curtains():
-    return jsonify({'curtains':kitchenroom['curtains']})
+        kitchenroom_curt_var=bool_to_str(kitchenroom['curtains']),
+        kitchenroom_lights_var=bool_to_str(kitchenroom['lights']),
+        kitchenroom_temp_var=kitchenroom['temp'],
+    )
 
-@app.route('api/bedroom/temp', methods=['GET'])
-def temp():
-    return jsonify({'temp':kitchenroom['temp']})
+####################LIVINGROOM###########################
+@app.route('/api/livingroom/<module>')
+def livingroom_cond(module):
+    return jsonify({f'{module}':livingroom[f'{module}']})
 
-#########################BEDROOM###########################
-@app.route('/api/bedroom/lights', methods=['GET'])
-def lights():
-    return jsonify({'lights':bedroom['lights']})
+####################KITCHENROOM###########################
+@app.route('/api/kitchenroom/<module>')
+def kitchenroom_cond(module):
+    return jsonify({f'{module}':kitchenroom[f'{module}']})
 
-@app.route('/api/bedroom/curtains', methods=['GET'])
-def curtains():
-    return jsonify({'curtains':bedroom['curtains']})
-
-@app.route('/api/bedroom/temp', methods=['GET'])
-def temp():
-    return jsonify({'temp':bedroom['temp']})
-
-
-#########################LIVINGROOM###########################
-@app.route('/api/livingroom/lights', methods=['GET'])
-def lights():
-    return jsonify({'lights':livingroom['lights']})
-
-@app.route('/api/livingroom/curtains', methods=['GET'])
-def curtains():
-    return jsonify({'curtains':livingroom['curtains']})
-
-@app.route('/api/livingroom/temp', methods=['GET'])
-def temp():
-    return jsonify({'temp':bedroom['temp']})
-
-
+####################BEDROOM###########################
+@app.route('/api/bedroom/<module>')
+def bedroom_cond(module):
+    return jsonify({f'{module}':bedroom[f'{module}']})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8090)
