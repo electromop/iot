fdf07d0462711843416aa1c5233870af04ef1e3c
electromop <user4567859@gmail.com>
2023-02-17 23:23:36 +0300

    experiments with dataclasses instead of dicts

---
+++ server/test.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+from typing import Any
+
+@dataclass
+class roomConds:
+    conds: Any
+    
+    def module(self, m):
+        return self.globals()[m]
+
+class rooms():
+    def roomCreate(string):
+        exec("%s = %d" % (string,0))
+        globals()[string] = roomConds
+        print('Комната добавлена!')
+
+    def moduleCreate(roomname, modulename):
+        exec("%s = %d" % (roomname,0))
+    
+print('1. Новый класс')
+print('2. Новый модуль')
+print('3. Изменить состояние модуля')
+print('4. Выйти')
+
+while True:
+    op = int(input())
+    if op == 1:
+        room_name = str(input())
+        rooms.roomCreate(room_name)
+    elif op == 4:
+        break
+
+print(globals())
